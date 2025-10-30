8d612082fd359e66b9330d9e7b4a81a860f4d8fb
Mauro <mauro_9_2@hotmail.com>
2025-10-30 14:56:43 -0500

    change title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,9 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-
-  title = "Face App";
-  constructor() {
-
-  }
-
+  title = "OCR App";
+  constructor() {}
 }
